8fcd0c48bb1882e143d09aff6c6e0a9e082e5c33
U-11 <mizu.w.w1.n23@gamil.com>
2020-11-08 20:44:04 +0900

    007チャットルーム一覧を出力

---
+++ db/db-challenge3/db-007/db-007.sql
@@ -0,0 +1,15 @@
+-- 以下の要件を満たしたチャットルーム一覧を出力
+-- ・削除されたチャットルームは表示しない
+-- ・全てのカラムを表示すること。ただし、*は使用しないこと
+-- ・概要がダイレクトチャットで終わっている
+-- ・ファイル送信が許可されているときは「許可」、許可されていないときは「禁止」と表示
+-- ・チャットルームidの昇順で表示
+
+SELECT id,name,description,
+CASE
+WHEN send_file=0 THEN '許可'
+WHEN send_file=1 THEN '禁止' END AS send_file,
+direct_chat,is_deleted,created_at,created_user_id,updated_at,updated_user_id
+FROM rooms
+WHERE is_deleted=0 AND description LIKE '%ダイレクトチャット'
+ORDER BY id;
